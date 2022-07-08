e1a3d2c777e8415c1d8c1a14cf5165d94d253fbd
lepahin <lepahin@ians.aero>
2022-07-08 12:39:05 +0300

    fix size

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
-import DateTimePicker from "../src";
+import DateTimePicker from '../src';
 import './index.sass';
 
 const DemoPage: React.FC = () => (
     <div className={'container'}>
-        <h2>Демо для компонента<br/>react-datetime-picker</h2>
+        <h2>
+            Демо для компонента
+            <br />
+            react-datetime-picker
+        </h2>
         <div>
-            <DateTimePicker pickerType={'datetime'} onChange={(value: any) => console.log(value)}/>
+            <DateTimePicker pickerType={'datetime'} onChange={(value: any) => console.log(value)} />
         </div>
         <div>
-            <DateTimePicker pickerType={'date'} onChange={(value: any) => console.log(value)} startYear={2005} endYear={2009}/>
+            <DateTimePicker
+                pickerType={'date'}
+                onChange={(value: any) => console.log(value)}
+                startYear={2005}
+                endYear={2009}
+            />
         </div>
         <div>
             <DateTimePicker pickerType={'time'} onChange={(value: any) => console.log(value)} />
